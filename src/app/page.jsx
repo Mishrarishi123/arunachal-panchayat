b45b2dfc5218a66a99d2f233c3246aaa648048f7
Rishi Mishra <mishrar1880@gmail.com>
2025-06-30 15:46:34 +0530

    Updated page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-import Header from "../components/Header/Header"
+import Header from '../components/Header/Header';
 import Hero from "../components/hero/Hero";
 import Map from "../components/map/Map";
 import Scheme from "../components/scheme/Scheme";
@@ -8,16 +8,14 @@ import About from "../components/about/About";
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <Header />
-      <main>
-        <Hero />
-        <Map />
-        <Scheme />
-        <About />
-        <Data />
-      </main>
+      <Hero />
+      <Map />
+      <Scheme />
+      <About />
+      <Data />
       <Footer />
-    </div>
+    </main>
   );
 }
